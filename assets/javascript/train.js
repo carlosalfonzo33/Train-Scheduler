7d9e47e315d388ba6da76f9ba076c21bafb5d75a
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-06-10 22:48:29 -0700

    saving js file

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -12,9 +12,53 @@ $(document).ready(funcion() {
   };
   firebase.initializeApp(config);
 
+var database = firebase.database();
+
+	var name = "";
+	var destination = "";
+	var startTime = "";
+	var frequency = "";
+	var childSnapshotArray = [];
 
 
+$("#add-train").on("click", function() {
+	name = $("#train-name").val().trim();
+	destionation = $("#destination").val().trim();
+	startTime = $("#start-time").val();
+	frequency = $("#frequency").val().trim();
 
+	database.ref().push({
+		name: name,
+		destination: destionation,
+		startTime: moment(startTime, "HH:mm").format("HH:mm"),
+		frequency: +frequency
+	});
+
+	$("#new-train-form")[0].reset();
+
+	return false;
+
+										});
+
+database.ref().on("child_added", function(childSnapshot) {
+
+	childSnapshotArray.push(childSnapshot.val());
+	render();
+},
+	function(errorObject) {
+		console.log("Errors handled: " + errorObject.code);
+	});
+
+fucntion minutesAway(startTime, frequency) {
+	var currentTime = moment();
+	var startTimeMinutes = currentTime.diff(momment(startTime, "HH:mm"), "minutes");
+	var numberOfTrains = Math.floor(startTimeMinutes / frequency);
+	var lastTrainTime = (numberOfTrains * frequency);
+
+}
+
+
+});
 
 
 });
