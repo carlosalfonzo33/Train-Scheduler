fc4b323c5c54b8ff293626195176d12acb38345b
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-06-10 20:58:52 -0700

    saving js file

---
+++ assets/javascript/train.js
@@ -0,0 +1,20 @@
+//document load
+$(document).ready(funcion() {
+
+	//Initalize firebase
+	 var config = {
+    apiKey: "AIzaSyD2S-x_cyfJFbjC_ArK9DV705A4uWclI1w",
+    authDomain: "train-scheduler-d4a50.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d4a50.firebaseio.com",
+    projectId: "train-scheduler-d4a50",
+    storageBucket: "train-scheduler-d4a50.appspot.com",
+    messagingSenderId: "513474582365"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+});
