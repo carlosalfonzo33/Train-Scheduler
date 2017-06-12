b6e65c6228e0101df427780a58e598cc229d375d
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-06-12 12:10:36 -0700

    firebase push now works

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,64 +1,44 @@
-//document load
-$(document).ready(funcion() {
+$(document).ready(function() {
 
-	//Initalize firebase
-	 var config = {
-    apiKey: "AIzaSyD2S-x_cyfJFbjC_ArK9DV705A4uWclI1w",
-    authDomain: "train-scheduler-d4a50.firebaseapp.com",
-    databaseURL: "https://train-scheduler-d4a50.firebaseio.com",
-    projectId: "train-scheduler-d4a50",
-    storageBucket: "train-scheduler-d4a50.appspot.com",
-    messagingSenderId: "513474582365"
-  };
-  firebase.initializeApp(config);
+    //initalize firebase
+    var config = {
+        apiKey: "AIzaSyD2S-x_cyfJFbjC_ArK9DV705A4uWclI1w",
+        authDomain: "train-scheduler-d4a50.firebaseapp.com",
+        databaseURL: "https://train-scheduler-d4a50.firebaseio.com",
+        projectId: "train-scheduler-d4a50",
+        storageBucket: "train-scheduler-d4a50.appspot.com",
+        messagingSenderId: "513474582365"
+    };
+    firebase.initializeApp(config);
 
-var database = firebase.database();
+    var database = firebase.database();
 
-	var name = "";
-	var destination = "";
-	var startTime = "";
-	var frequency = "";
-	var childSnapshotArray = [];
+    // intialize the global variables. 
 
+    var name = "";
+    var destination = "";
+    var startTime = "";
+    var frequency = "";
+    var childSnapshotArray = [];
 
-$("#add-train").on("click", function() {
-	name = $("#train-name").val().trim();
-	destionation = $("#destination").val().trim();
-	startTime = $("#start-time").val();
-	frequency = $("#frequency").val().trim();
+    //capture the value of the input boxes on click of the submit button and set equal to the global variables
+    $("#add-train").on("click", function() {
+        name = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        startTime = $("#start-time").val();
+        frequency = $("#frequency").val().trim();
 
-	database.ref().push({
-		name: name,
-		destination: destionation,
-		startTime: moment(startTime, "HH:mm").format("HH:mm"),
-		frequency: +frequency
-	});
+        //push the input values to firebase as an object
+        database.ref().push({
+            name: name,
+            destination: destination,
+            startTime: moment(startTime, "HH:mm").format("HH:mm"),
+            frequency: +frequency
 
-	$("#new-train-form")[0].reset();
-
-	return false;
-
-										});
-
-database.ref().on("child_added", function(childSnapshot) {
-
-	childSnapshotArray.push(childSnapshot.val());
-	render();
-},
-	function(errorObject) {
-		console.log("Errors handled: " + errorObject.code);
-	});
-
-fucntion minutesAway(startTime, frequency) {
-	var currentTime = moment();
-	var startTimeMinutes = currentTime.diff(momment(startTime, "HH:mm"), "minutes");
-	var numberOfTrains = Math.floor(startTimeMinutes / frequency);
-	var lastTrainTime = (numberOfTrains * frequency);
-
-}
+        });
+        //reset the form
+        $("#new-train-form")[0].reset();
 
 
-});
-
-
-});
+        return false;
+   
